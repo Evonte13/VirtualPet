a68c364e90e90fac0909b94962672d312728659d
evonte dunn <evontedunn@gmail.com>
2017-02-27 21:14:48 -0500

    first commit

---
+++ VirtualPet/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            VirtualPet dragonPet = new VirtualPet();
+            string option;
+           
+            Console.WriteLine("Welcome to Dragonland");
+           
+            int typeOfDragon;
+            Console.WriteLine("What kind of Dragon do you want to be?");
+            Console.WriteLine("Press 1 for Dragonite, \nPress 2 for Black dragon, \nPress 3 for White dragon,\nor Press 4 for Pink dragon");
+            typeOfDragon = int.Parse(Console.ReadLine());
+            
+            if(typeOfDragon == 1)
+            {
+                Console.WriteLine("Dragonite");
+            }
+            else if(typeOfDragon== 2)
+            {
+                Console.WriteLine("Black Dragon");
+            }
+            else if (typeOfDragon == 3)
+            {
+                Console.WriteLine("White Dragon");
+            }
+            else if (typeOfDragon == 4)
+            {
+                Console.WriteLine("Pink Dragon");
+            }
+            else
+            {
+                Console.WriteLine("Gumpy the Dragon");
+            }
+            
+
+
+
+            do
+            {
+               
+                dragonPet.Stats();
+                Console.WriteLine("What do you want to do? Earn Money, Drink soda, Eat, or Sleep?");
+                Console.WriteLine("Press 1 to earn money, 2 to drink soda, 3 to eat, or 4 to sleep");
+                string answer = Console.ReadLine();
+
+                if (answer == "1")
+                {
+                    dragonPet.Raise();
+
+                }
+
+                else if (answer == "2")
+                {
+                    dragonPet.DrinkSoda();
+                }
+
+                else if (answer == "3")
+                {
+                    dragonPet.Eat();
+                }
+
+                else if (answer == "4")
+                {
+                    dragonPet.Sleep();
+                }
+
+                else
+                {
+                    dragonPet.DrinkSoda();
+
+                }
+
+                dragonPet.Stats();
+
+                Console.WriteLine("Do you want to go again? \nType yes to go again!");
+                option = Console.ReadLine();
+                option = option.ToUpper();
+             
+
+            } while (option == "Yes");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ VirtualPet/VirtualPet.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class VirtualPet
+    {
+
+
+         string typeOfDragon;
+         int money;
+         int hunger;
+         int happiness;
+         int sleepiness;
+         string name;
+         string dragonName;
+
+
+
+        //Constructors
+        
+        public VirtualPet()
+        {
+            this.typeOfDragon ="dora";
+            this.money = 5;
+            this.hunger = 5;
+            this.happiness = 2;
+            this.name = dragonName;
+            this.sleepiness = 2;
+        }
+
+        #region PROPERTIES
+        //Properties
+
+        public string TypeOfDragon
+        {
+            get { return this.typeOfDragon; }
+            set { this.typeOfDragon = value; }
+        }
+        public int Money
+        {
+            get { return this.money; }
+            set { this.money = value; }
+        }
+        public int Hunger
+        {
+            get { return this.hunger; }
+            set { this.hunger = value; }
+        }
+
+        public int Happiness
+        {
+            get { return this.happiness; }
+            set { this.hunger = value; }
+        }
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int Sleepiness
+        {
+            get { return this.sleepiness; }
+            set { this.sleepiness = value; }
+        }
+
+        public string DragonName
+        {
+            get { return this.dragonName; }
+            set { this.dragonName = value; }
+        }
+        #endregion
+
+
+        #region METHODS
+        //Methods
+        public void Raise()
+        {
+            money = money + 5;
+        }
+
+        public void Eat()
+        {
+            hunger = hunger - 1;
+            money = money - 2;
+            happiness = happiness + 1;
+        }
+
+        public void DrinkSoda()
+        {
+            money = money - 5;
+            happiness = happiness + 2;
+            sleepiness = sleepiness + 2;
+        }
+
+        public void Sleep()
+        {
+            sleepiness = sleepiness - 5;
+            hunger = hunger + 2;
+            happiness = happiness + 1;
+        }
+
+        public void Stats()
+        {
+            
+            Console.WriteLine("Money: " + this.money);
+            Console.WriteLine("Hunger: " + this.hunger);
+            Console.WriteLine("Happiness: " + this.happiness);
+            Console.WriteLine("Sleepiness: " + this.sleepiness);
+        }
+
+        
+
+
+        #endregion 
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
+ 
